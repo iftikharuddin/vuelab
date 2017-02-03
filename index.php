7df0155a0892a97d22bfd13299e8525128f2f106
Iftikhar uddin <iftipro@gmail.com>
2017-02-03 11:55:02 +0500

    Learning Vuejs

---
+++ index.php
@@ -0,0 +1,14 @@
+!<!DOCTYPE html>
+<html>
+<head>
+	<title>Vue Learning</title>
+</head>
+<body>
+<div id="app">
+	<p>{{title}}</p>
+</div>
+	<script type="text/javascript" src="https://unpkg.com/vue@2.1.10/dist/vue.js"></script>
+	<script type="text/javascript" src="script.js"></script>
+
+</body>
+</html>
